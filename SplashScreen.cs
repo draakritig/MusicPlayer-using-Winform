6427fe8cbd2959fcb64297d3f74a82073a36eb85
Aakriti Gupta <draakritig@gmail.com>
2020-08-18 13:12:47 -0400

    Second commit

---
+++ SplashScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MusicPlayer
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+
+            //Sets size of splashscreen to size of background image and re-centers splash screen
+            ClientSize = BackgroundImage.Size;
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void SplashScreen_Load(object sender, EventArgs e)
+        {           
+            //determines location of loadingBar and loadingMessage based on new screen size
+            loadingBar.Location = new Point((BackgroundImage.Width / 2) - 85, BackgroundImage.Height - 30);
+            loadingMessage.Location = new Point((BackgroundImage.Width / 2) - 35, BackgroundImage.Height - 50);
+        }
+    }
+}
